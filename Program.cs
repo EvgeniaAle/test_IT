a0f62f30dd7bb6d7a64d1d7d913c4fcea862e2c9
unknown <aleksanova.evgenia@gmail.com>
2023-03-11 17:44:26 +0300

    решение

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.//
+string [] M;
+Console.WriteLine ("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split();
+var result = new string [M .Length];
+var realSize = 0;
+foreach (var value in M)
+{
+    if (value.Length<=3)
+    {result[realSize] = value;
+    realSize++;
+    }
+}
+Console.WriteLine(String.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
